ab79a61483fb344dc548423640f505b228035827
ali <ali.akeelqq123@gmail.com>
2024-09-03 15:12:22 +0300

    clean up find_random_word code

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,16 +1,13 @@
 require 'pry-byebug'
 
-file_string = 'google-10000-english-no-swears.txt'
 file_array = File.readlines('google-10000-english-no-swears.txt')
-
-random = rand(file_array.size)
 
 class Hangman
   def initialize(dict)
     @dict = dict
   end
 
-  def find_random_word(file,file_array)
+  def find_random_word(file_array)
     # make flag for if word is found, set to false
     # while the flag is false do:
     # create random number
@@ -22,7 +19,7 @@ class Hangman
     word_found = false
     while word_found == false
     p random = rand(file_array.size)
-    File.foreach(file).with_index do |line,index|
+    file_array.each_with_index do |line,index|
       if index == random - 1
         word = line.chomp
         if word.length >= 5 && word.length <= 12
@@ -37,5 +34,5 @@ class Hangman
 end
 
 
-game = Hangman.new(file_string)
-game.find_random_word(file_string,file_array)
+game = Hangman.new("test")
+game.find_random_word(file_array)
