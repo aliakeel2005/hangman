057707c4444c6d8bb2c2cd9b8d1cb04167b38711
ali <ali.akeelqq123@gmail.com>
2024-09-04 10:57:24 +0300

    clean up hangman class

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,13 +1,16 @@
 require 'pry-byebug'
 
 class Hangman
-  def initialize; end
-  @@file_array = File.readlines('google-10000-english-no-swears.txt')
+  def initialize
+    @file_array = File.readlines('google-10000-english-no-swears.txt')
+    @word = nil
+  end
+
   def does_word_match?(random)
-    line = @@file_array[random - 1]
-    word = line.chomp
-    if word.length >= 5 && word.length <= 12
-      p word
+    line = @file_array[random - 1]
+    @word = line.chomp
+    if @word.length >= 5 && @word.length <= 12
+      p @word
       return true
     end
     false
@@ -23,11 +26,17 @@ class Hangman
     # if not, redo process
     word_found = false
     while word_found == false
-      p random = rand(@@file_array.size)
+      p random = rand(@file_array.size)
       word_found = does_word_match?(random)
     end
+  end
+
+  def guess_word
+    player_guess = gets.chomp
+    p @word.split('')
   end
 end
 
 game = Hangman.new
 game.find_random_word
+game.guess_word
